7b258c43ca049688358e500e22861e80f5676a6e
Shamim Mahbub <60071360+shamim237@users.noreply.github.com>
2023-01-25 23:57:24 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from scrap import extract
+from paraphrase import para
+from summary import summarize
+
+
+st.title("Let's Summarize!")
+link = st.text_input("Enter a product link from amazon....")
+print(link)
+def process():
+    data = extract(link)
+    #print(data)
+    paras = para(data)
+    summ = summarize(paras)
+    st.success(summ)
+st.button('Extract', on_click=process)
+st.text("Here is the product description...")
