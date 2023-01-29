f811d3f5c9b3747bd4e2fec5977d10c2ef83b306
Shamim Mahbub <60071360+shamim237@users.noreply.github.com>
2023-01-29 14:57:09 +0600

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,30 +1,14 @@
 import time
-from selenium import webdriver
 from selenium.webdriver import Chrome
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 
 def extract(link):
     url = link
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(
-        options=firefoxOptions,
-        service=service,
-    )
-    driver.get(url)
-    data = driver.find_element(By.ID,"aplus_feature_div")
+    driver_path = "./chromedriver.exe"
+    browser = Chrome(executable_path = driver_path)
+    browser.get(url)
+    data = browser.find_element(By.ID,"aplus_feature_div")
     data = data.text
     data = data.split("\n")
-    time.sleep(2)
+    time.sleep(1)
     return data
-
-# ss = extract("https://www.amazon.com/dp/B09B9TB61G?th=1")
-# print(ss)
