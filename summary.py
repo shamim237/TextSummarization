9805d4dd7f4c3c5595f4ec4108e7a9b6182c1b2f
Shamim Mahbub <60071360+shamim237@users.noreply.github.com>
2023-01-29 14:57:44 +0600

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,13 +1,24 @@
-import torch
-from transformers import PegasusForConditionalGeneration, AutoTokenizer
+import nltk
+import streamlit as st
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.summarizers.lex_rank import LexRankSummarizer
 
-def summarize(passage):
-    txt = " ".join(passage)
-    model_name = 'google/pegasus-cnn_dailymail'
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
-    batch = tokenizer(txt, truncation=True, padding='longest', return_tensors="pt").to(device)
-    translated = model.generate(**batch)
-    summy = tokenizer.batch_decode(translated, skip_special_tokens=True)
-    return summy
+@st.cache(allow_output_mutation=True, ttl=48*3600)
+def dwnld_lib():
+    nltk.download('punkt')
+    
+dwnld_lib()
+
+def text_summary(text):
+    para = " ".join(text)
+    # Create a plaintext parser and tokenizer
+    parser = PlaintextParser.from_string(para, Tokenizer("english"))
+    # Create a LexRank summarizer
+    summarizer = LexRankSummarizer()
+    # Summarize the text and print the results
+    summ = []
+    for sentence in summarizer(parser.document, 4):
+        summy =  str(sentence).capitalize()
+        summ.append(summy)
+    return summ
